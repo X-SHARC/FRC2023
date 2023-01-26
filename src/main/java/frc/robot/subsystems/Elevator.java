c39394731573040c2604a9d5433b046d7947937d
Berra Eylül Toprak <90389241+berraeylultoprak@users.noreply.github.com>
2023-01-26 17:32:29 +0300

    update

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,67 +4,52 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase {
-  /** Creates a new Elevator. */
   WPI_TalonFX elevatorMasterMotor = new WPI_TalonFX(Constants.ELEVATOR_SLAVE_ID);
   WPI_TalonFX elevatorSlaveMotor = new WPI_TalonFX(Constants.ELEVATOR_MASTER_ID);
 
   DigitalInput topLimitSwitch = new DigitalInput(0);
   DigitalInput bottomLimitSwitch = new DigitalInput(1);
 
-  /** Creates a new Elevator. */
-  public Elevator() {
-    elevatorSlaveMotor.setInverted(false);
-    elevatorMasterMotor.setInverted(true);
-    elevatorSlaveMotor.follow(elevatorSlaveMotor);
-  }
+  private double kP = 1;
+  private double kI = 2;
+  private double kD = 3;
+  private double kG = 4;
 
-double gearCircumference = 4.8514 * Math.PI;
+  private double kS = 1;
+  private double kV = 2;
+  private double kA = 3;
 
-  private double kP = 0.11533;
-  private double kI = 0.0;
-  private double kD = 0.0;
-
-  private double kS = 0.74791;
-  private double kV = 0.11447;
-  private double kA = 0.0063222;
-  private double kG = 0.0063222;
-
-  ElevatorFeedforward feedforward = new ElevatorFeedforward(kS, kG, kV, kA);
-
-  private double PIDOutput;
-  private double feedForwardOutput;
   private double distance;
-
-  public PIDController elevatorPID = new PIDController(kP, kI, kD);  
+  private double feedForwardOutput;
+  private double PIDOutput;
+  
+  double gearCircumference = 4.8514 * Math.PI;
 
   double error;
   double output;
 
+  public PIDController elevatorPID = new PIDController(kP, kI, kD);
+  ElevatorFeedforward feedForward = new ElevatorFeedforward(kS, kG, kV, kA);
+  /** Creates a new Elevator. */
 
-  public void resetEncoder() {
-    distance = 0.0; 
-    // encoder in raw datasını donüş sayısına dönüştürme
-  // dönüş sayısı distance a 
+  public Elevator() {
+    elevatorSlaveMotor.setInverted(false);
+    elevatorMasterMotor.setInverted(true);
+    elevatorSlaveMotor.follow(elevatorMasterMotor);
   }
 
-  //merhaba harun hocam saygılar
-
-  public void setDistance(double setpoint) {
-    double distance = elevatorMasterMotor.getSelectedSensorPosition();
-    distance = (distance/2048.0) * gearCircumference;
-    elevatorPID.setTolerance(100);
-    PIDOutput = elevatorPID.calculate(60., setpoint);
-    feedForwardOutput = feedforward.calculate(60.);
-
-    //output = (PIDOutput + feedForwardOutput) / RobotController.getBatteryVoltage();
-    output = (PIDOutput + feedForwardOutput);
-    elevatorMasterMotor.set(ControlMode.PercentOutput,output);
-  }
-  
-  
   /*   if (SensorValue(topLimitSwitch) == true) {
       elevatorMasterMotor.set();
     } else {
@@ -80,13 +65,32 @@ double gearCircumference = 4.8514 * Math.PI;
       if ()
     }   */ 
 
-   public void elevatorUp(){
+    
+    public void resetEncoder(){
+      elevatorMasterMotor.setSelectedSensorPosition(0);
+    }
+
+    public void setDistance(double setpoint){
+      double distance = elevatorMasterMotor.getSelectedSensorPosition();
+      distance = (distance/2048.0) * gearCircumference;
+
+      elevatorPID.setTolerance(100);
+      PIDOutput = elevatorPID.calculate(60., setpoint);
+      feedForwardOutput = feedForward.calculate(60.0);
+      output = (PIDOutput + feedForwardOutput) / RobotController.getBatteryVoltage();
+      elevatorMasterMotor.set(ControlMode.PercentOutput, output);
+    }
+
+
+    public void elevatorUp(){
+   //   if (topLimitSwitch.get() == false){
      elevatorMasterMotor.set(ControlMode.PercentOutput, 0.5);
-      }
+      } //}
 
     public void elevatorDown(){
+  //    if (bottomLimitSwitch.get() == false){
       elevatorMasterMotor.set(ControlMode.PercentOutput, -0.5);
-    }
+    } //}
 
     public void stop(){
       elevatorMasterMotor.set(ControlMode.PercentOutput, 0.0);
@@ -94,8 +98,9 @@ double gearCircumference = 4.8514 * Math.PI;
 
   @Override
   public void periodic() {
-    SmartDashboard.putNumber("Elevator Encoder:", distance);
+    SmartDashboard.putNumber("Elevator Encoder Value:", distance);
     // This method will be called once per scheduler run
   }
-}
 
+
+}
