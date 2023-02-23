5489e475543fccb6265816efccdb2a67dc682b8d
Berra Eylül Toprak <septemberdirt23@gmail.com>
2023-02-23 12:45:43 +0300

    ll ilk update

---
+++ src/main/java/frc/robot/lib/vision/LimelightVision.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.lib.vision;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimelightVision extends SubsystemBase {
+  /** Creates a new LimelightVision. */
+  public LimelightVision() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
