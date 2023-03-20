49f16b09f19f090f8363c4fbcca7bafd48a622e2
Burak Demirelli <54762910+burakdemirelli@users.noreply.github.com>
2023-03-20 08:27:31 +0300

    fixed overloaded swerve drive contructors

--- src/main/java/frc/robot/commands/Swerve/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/Swerve/SwerveDriveCommand.java
@@ -19,23 +19,16 @@ public class SwerveDriveCommand extends CommandBase {
   Swerve swerveSubsystem;
   Joystick driver;
 
-  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-  // TODO not using them currently, try out and see if you want to keep them for comp
   private final SlewRateLimiter xSpeedLimiter = new SlewRateLimiter(2.5);
   private final SlewRateLimiter ySpeedLimiter = new SlewRateLimiter(2.5);
   private final SlewRateLimiter rotLimiter = new SlewRateLimiter(4.5);
 
   double scale = 1;
-  double scale2= 0.4;
-  double slowScale = 0.2;
 
-  
-    /** Creates a new SwerveDriveCommand. */
     public SwerveDriveCommand(Swerve sw, XboxController joystick) {
       this.swerveSubsystem = sw;
       this.joystick = joystick;
-      // Use addRequirements() here to declare subsystem dependencies.
-      addRequirements(sw);
+      addRequirements(swerveSubsystem);
     }
 
 
@@ -46,12 +39,10 @@ public class SwerveDriveCommand extends CommandBase {
       addRequirements(swerveSubsystem);
     }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     scale = RobotState.isElevated() ? 0.45 : 1;
